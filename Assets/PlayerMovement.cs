1f41ba09fde018e50ed7eed80b314db95997337e
phazeless <felixlienaldi@gmail.com>
2018-02-28 18:11:25 +0700

    Project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[HideInInspector] public float AngleMinimumX = -45f;
+    [HideInInspector] public float AngleMaximumX = 45f;
+    [HideInInspector] public float RotationX;
+    [HideInInspector] public float RotationY;
+
+    public float Sensitivy;
+
+    public float Speed;
+
+    public List<KeyCode> Key = new List<KeyCode>();
+
+    public Rigidbody Rb;
+    public Camera cam;
+
+	void Start () {
+        Cursor.visible = false;
+
+	}
+	
+
+	void Update () {
+        RotationMove();
+        TranslationMove();
+    }
+
+    void RotationMove()
+    {
+        RotationX = Mathf.Clamp(-Input.mousePosition.y * Sensitivy, AngleMinimumX, AngleMaximumX);
+        RotationY = Input.mousePosition.x * Sensitivy;
+        cam.transform.rotation = Quaternion.Euler(new Vector3(RotationX, RotationY, 0f));
+       
+    }
+
+    void TranslationMove()
+    {
+         
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rb.velocity = new Vector3(0f, 0f, Speed);
+            
+        }   
+    }
+}
