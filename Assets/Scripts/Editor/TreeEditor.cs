26bbc77bdffb4183e214922f5855e348640c3bf4
phazeless <32062035+phazeless@users.noreply.github.com>
2018-05-30 12:10:51 +0700

    UpdateTree

---
+++ Assets/Scripts/Editor/TreeEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TreeScript))]
+public class TreeEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TreeScript Tree = (TreeScript)target; 
+
+        if(GUILayout.Button("Generate Tree"))
+        {
+            Tree.GenerateTree();
+            Debug.Log("Generate Complete!");
+        }
+    }
+}
---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    public GameObject Tree;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateTree()
+    {
+        for(int i = 0; i < 900; i++)
+        {
+            //46 27, 6 69
+            GameObject tree = Instantiate(Tree, new Vector3(Random.Range(-100f, 5f), 30.8f, Random.Range(-120f, 120f)), Quaternion.identity);
+            tree.transform.parent = gameObject.transform;
+        }
+        
+    }
+}
